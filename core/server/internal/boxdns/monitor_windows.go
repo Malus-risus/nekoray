e5d492633ae09ee025864eaccca9026f05c85d14
Το μοχθηρό ^_^ <cryberghost@gmail.com>
2025-07-24 12:53:51 +0800

    Revert "Update monitor_windows.go"
    This reverts commit 54025ea6f5951190dccaeff1f46d8cf4793d6eff.

--- core/server/internal/boxdns/monitor_windows.go
+++ core/server/internal/boxdns/monitor_windows.go
@@ -5,6 +5,7 @@ import (
 	tun "github.com/sagernet/sing-tun"
 	"github.com/sagernet/sing/common/control"
 	logger2 "github.com/sagernet/sing/common/logger"
+	"log"
 	"nekobox_core/internal/boxdns/winipcfg"
 )
 
@@ -72,8 +73,7 @@ func getFirstDNS(luid winipcfg.LUID) string {
 }
 
 func getNameServersForInterface(luid winipcfg.LUID) ([]string, error) {
-	nameserversV4 := make([]string, 0, 4)
-	nameserversV6 := make([]string, 0, 4)
+	nameservers := make([]string, 0, 4)
 	nsAddrs, err := luid.DNS()
 	if err != nil {
 		return nil, err
@@ -84,25 +84,16 @@ func getNameServersForInterface(luid winipcfg.LUID) ([]string, error) {
 			isSystemDNSAltered = true
 		}
 		if server.IsValid() && server.String() != setMarkAddr && server.String() != dhcpMarkAddr {
-			if server.Is4() {
-				nameserversV4 = append(nameserversV4, server.String())
-			} else {
-				nameserversV6 = append(nameserversV6, server.String())
-			}
+			nameservers = append(nameservers, server.String())
 		}
 	}
-	if isSystemDNSAltered && len(nameserversV4) > 0 && nameserversV4[0] == "127.0.0.1" {
-		nameserversV4 = nameserversV4[1:]
+	if isSystemDNSAltered && len(nameservers) > 0 && nameservers[0] == "127.0.0.1" {
+		nameservers = nameservers[1:]
 	}
 
-	if len(nameserversV4) > 0 {
-		return nameserversV4, nil
+	if len(nameservers) == 0 {
+		log.Println("getNameServersForInterface: no nameservers found")
 	}
 
-	if len(nameserversV6) > 0 {
-		return nameserversV6, nil
-	}
-
-	log.Println("getNameServersForInterface: no nameservers found")
-	return nil, nil
+	return nameservers, nil
 }
